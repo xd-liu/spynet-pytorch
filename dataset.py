f7ce7fe5b36f0195d7b2d213d249db6b1fb7637d
Xudong Liu <liuxd1215@gmail.com>
2020-08-05 01:44:16 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -22,8 +22,8 @@ class Mdata(Dataset):
     def __getitem__(self, index):
         img_first_name = os.path.join(self.data_root, self.data_list[index][0])
         img_second_name = os.path.join(self.data_root, self.data_list[index][1])
-        tenFirst = torch.FloatTensor(numpy.ascontiguousarray(numpy.array(PIL.Image.open(arguments_strFirst))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0)))
-        tenSecond = torch.FloatTensor(numpy.ascontiguousarray(numpy.array(PIL.Image.open(arguments_strSecond))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0)))
+        tenFirst = torch.FloatTensor(numpy.ascontiguousarray(numpy.array(PIL.Image.open(img_first_name))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0)))
+        tenSecond = torch.FloatTensor(numpy.ascontiguousarray(numpy.array(PIL.Image.open(img_second_name))[:, :, ::-1].transpose(2, 0, 1).astype(numpy.float32) * (1.0 / 255.0)))
         return tenFirst.view(3, 720, 1280), tenSecond.view(3, 720, 1280)
 
     def read_lists(self, data_list):
